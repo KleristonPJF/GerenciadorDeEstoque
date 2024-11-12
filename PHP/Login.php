605432e1851a3370c71aea43ce01153445e0bc2e
KleristonPJF <Kleristonferreira18@gmail.com>
2024-11-12 14:35:32 -0500

    Finalmente, Login fuincionando

--- PHP/Login.php
+++ PHP/Login.php
@@ -3,34 +3,34 @@
     $usuario = $_POST['Usuario'];
     $senha = $_POST['Senha'];
 
-    try{
+    try {
         $conn = new PDO("mysql:host=localhost;dbname=Estoque", "root", "");
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        $sql = "INSERT INTO Usuario(usuario, senha) VALUES (:usuario, :senha)";
+        $sql = "SELECT id, usuario, senha FROM Usuario WHERE usuario = :usuario";
         $resp = $conn->prepare($sql);
 
         $resp->bindParam(':usuario', $usuario);
         $resp->execute();
 
-        $dados = $resp->fetchAll();
+        $dados = $resp->fetch();
 
-        if(count($dados) > 0){
-           if (password_verify($senha, $dados['senha'])) {
-            $id_usuario = $dados[0]['id'];
-            session_start();
-            $_SESSION['id_usuario'] =  $id_usuario;            
-            header('Location: ../Portal.html');
-           }else{
-            header('Location: ../Login.html');
-           }             
-        }else{
-            header('Location: ../Login.html');
+        if ($dados) {
+            if (password_verify($senha, $dados['senha'])) {
+		        $id_usuario = $dados['id'];
+                session_start();
+                $_SESSION['id_usuario'] = $id_usuario;
+                echo $id_usuario; 
+                header('Location: ../Portal.html');
+            } else {
+                header('Location: ../Login.html?erro=senha');
+            }
+        } else {
+            header('Location: ../Login.html?erro=usuario');
         }
 
-
-    }catch(Exception $erro){
-
+    } catch (Exception $erro) {
+        echo "Erro: " . $erro->getMessage();
     }
 
 ?>
